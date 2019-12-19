8dcc5a7944c62085aba22539cf60ec053160b44a
kakk91 <kaakk9991@gmail.com>
2019-12-19 19:16:06 +0900

    Built environment doing

---
+++ kanban-app/config/dev.env.js
@@ -0,0 +1,12 @@
+// 'use strict'
+// const merge = require('webpack-merge')
+// const prodEnv = require('./prod.env')
+
+// module.exports = merge(prodEnv, {
+//   NODE_ENV: '"development"'
+// })
+// 開発環境:開発向けの設定。本番環境の変数値をマージによって継承
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  DEBUG_MODE: true //`true`で上書き
+})
